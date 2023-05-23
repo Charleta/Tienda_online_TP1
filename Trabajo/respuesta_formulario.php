cadbdecda3dfec0930dda400e8186cd1a8bc453f
carlos ortega <sr.carlosortega@gmail.com>
2023-05-22 23:21:12 -0300

    mensaje nuevo

--- Trabajo/respuesta_formulario.php
+++ Trabajo/respuesta_formulario.php
@@ -6,11 +6,17 @@ $libro = $_GET['libro'];
 ?>
 
 <?php include ('secciones/headers.php')?>
+
+    <style>
+        main {
+            margin-top: 150px;
+        }
+    </style>
     <main>
         <div class="formularioenviado">
-            <p>¡Mensaje enviado exitosamente!</p>
+            <p>¡Mensaje enviado Correctamente!</p>
             <h1>Muchas gracias por contactarnos, <?= $nombre ?></h1>
-            <p>Recibimos tu solicitud sobre <span><?= $libro ?></span>, en los próximos días nos pondremos en contacto contigo para notificarte cuando el libro esté disponible.</p>
+            <p>Recibimos tu solicitud sobre <span><?= $libro ?></span>, en los próximos días nos pondremos en contacto contigo para notificarte cuando el producto este disponible y su costo.</p>
             <p>La notificación será enviada a: <span><?= $email ?></span></p>
 
             <a href="index.php" class="btn">Volver a inicio</a>
