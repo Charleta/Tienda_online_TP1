ea196f2e0451f605f1098611e54622b5a6248c61
carlos ortega <sr.carlosortega@gmail.com>
2023-05-22 23:23:01 -0300

    actualizar enviado

--- Trabajo/respuesta_formulario.php
+++ Trabajo/respuesta_formulario.php
@@ -13,7 +13,7 @@ $producto = $_GET['libro'];
         }
     </style>
     <main>
-        <div class="formularioenviado">
+        <div>
             <p>¡Mensaje enviado Correctamente!</p>
             <h1>Muchas gracias por contactarnos, <?= $nombre ?></h1>
             <p>Recibimos tu solicitud sobre <span><?= $producto ?></span>, en los próximos días nos pondremos en contacto contigo para notificarte cuando el producto este disponible y su costo.</p>
