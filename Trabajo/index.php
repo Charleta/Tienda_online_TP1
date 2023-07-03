1d9f3cf8a99d55b426f3ec9d14ccd114c576b4fa
carlos ortega <sr.carlosortega@gmail.com>
2023-07-03 01:32:50 -0300

    mejoras

--- Trabajo/index.php
+++
@@ -1,9 +0,0 @@
-
-<?php $vista = $_GET['seccion'];
-    $vista = $vista ? $vista : 'home'; ?>
-
-<?php include ('secciones/headers.php')?>
-
-<?php include("vistas/$vista.php") ?>
-
-<?php include ('secciones/footer.php') ?>
